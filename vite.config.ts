a5633ce0527e65b7781d7ed8ada4411bc8afca08
jycouet <jycouet@gmail.com>
2025-09-05 11:51:19 +0200

    prepare things

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,26 @@
-import {sveltekit} from '@sveltejs/kit/vite';
+import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
-import Icons from 'unplugin-icons/vite'
-import {defineConfig} from 'vite';
-import {kitRoutes} from "vite-plugin-kit-routes";
-import {stripper} from 'vite-plugin-stripper'
-
+import Icons from "unplugin-icons/vite";
+import { defineConfig } from "vite";
+import { kitRoutes } from "vite-plugin-kit-routes";
+import { stripper } from "vite-plugin-stripper";
 
 export default defineConfig({
-    plugins: [sveltekit(), tailwindcss(), kitRoutes(),
-        Icons({compiler: 'svelte'}),
-        stripper({ strip: ['BackendMethod'] }),
-    ],
+  plugins: [
+    sveltekit(),
+    tailwindcss(),
+    kitRoutes(),
+    Icons({ compiler: "svelte" }),
+    // stripper({ strip: ['BackendMethod'] }),
+  ],
 
-    //Exclude node modules from remult client side
-    // build: {
-    //     rollupOptions: {
-    //         external: ['fs', 'path','nodemailer', 'node-fetch'],
-    //     },
-    // },
-    // optimizeDeps: {
-    //     exclude: ['fs', 'path', 'nodemailer', 'node-fetch'],
-    // },
+  //Exclude node modules from remult client side
+  // build: {
+  //     rollupOptions: {
+  //         external: ['fs', 'path','nodemailer', 'node-fetch'],
+  //     },
+  // },
+  // optimizeDeps: {
+  //     exclude: ['fs', 'path', 'nodemailer', 'node-fetch'],
+  // },
 });
